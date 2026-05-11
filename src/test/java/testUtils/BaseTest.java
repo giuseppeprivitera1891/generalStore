d6c56d77b006330b5a495408a085a7336253f4d3
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-11 15:55:53 +0200

    Added chromedriver and apk, changed path for chromedriver and apk

--- src/test/java/testUtils/BaseTest.java
+++ src/test/java/testUtils/BaseTest.java
@@ -22,9 +22,9 @@ public class BaseTest {
 	@BeforeClass
 	public void configuration() throws MalformedURLException, URISyntaxException {
 		UiAutomator2Options options = new UiAutomator2Options();
-		options.setDeviceName("HOTWAV TAB R9 PLUS");
-	    options.setChromedriverExecutable("//home//giuseppeprivitera//eclipse-workspace//appium//src//test//java//resources//chromedriver");
-	    options.setApp("//home//giuseppeprivitera//eclipse-workspace//appium//src//test//java//resources//General-Store.apk");
+		options.setDeviceName("Pixel 8");
+	    options.setChromedriverExecutable("//home//giuseppeprivitera//eclipse-workspace//generalStore//src//test//java//resources//chromedriver");
+	    options.setApp("//home//giuseppeprivitera//eclipse-workspace//generalStore//src//test//java//resources//General-Store.apk");
 			
 	    driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
