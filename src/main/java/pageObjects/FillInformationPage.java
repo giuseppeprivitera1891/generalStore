102917536c70f637fff0a87b9f812b3e95e8418c
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-13 21:21:32 +0200

    Fix constructor method

--- src/main/java/pageObjects/FillInformationPage.java
+++ src/main/java/pageObjects/FillInformationPage.java
@@ -3,15 +3,19 @@ package pageObjects;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import utils.AndroidActions;
 
-public class FillInformationPage {
+public class FillInformationPage extends AndroidActions {
 	public AndroidDriver driver;
 
 	public FillInformationPage(AndroidDriver driver) {
+		super(driver);
 		this.driver = driver;
-		PageFactory.initElements(driver, this);
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 	
 	@AndroidFindBy(id="android:id/text1")
@@ -28,5 +32,12 @@ public class FillInformationPage {
 	
 	@AndroidFindBy(id="com.androidsample.generalstore:id/btnLetsShop")
 	private WebElement shopButton;
+	
+	public void selectCountry(String country) {
+		countrySelection.click();
+		scrollToText(country);
+		driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\""+ country + "\")")).click();
+	
+	}
 
 }
--- src/test/java/tests/TestSuite.java
+++ src/test/java/tests/TestSuite.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import pageObjects.FillInformationPage;
 import testUtils.BaseTest;
 
 public class TestSuite extends BaseTest {
@@ -17,9 +18,11 @@ public class TestSuite extends BaseTest {
 	
 	@Test
 	public void runSuite() {
-		driver.findElement(By.id("android:id/text1")).click();
-		scrollToText(country);
-		driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"" + country + "\")")).click();	
+		//driver.findElement(By.id("android:id/text1")).click();
+		//scrollToText(country);
+		//driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"" + country + "\")")).click();
+		FillInformationPage fillInformationPage = new FillInformationPage(driver);
+		fillInformationPage.selectCountry(country);
 		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rahul Shetty");	
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
 		
