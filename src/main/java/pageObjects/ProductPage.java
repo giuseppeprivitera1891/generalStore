912f3bdf5e9b8401d508a0943902e1e30ead903a
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-14 16:35:02 +0200

    Added selectProduct method

--- src/main/java/pageObjects/ProductPage.java
+++ src/main/java/pageObjects/ProductPage.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
@@ -22,11 +24,30 @@ public class ProductPage extends AndroidActions {
 	
 	String getProductTitle;
 	
+	@AndroidFindBy(id="com.androidsample.generalstore:id/productName")
+	private List<WebElement> productList;
+	
+	int countProduct;
+	
+	@AndroidFindBy(id="com.androidsample.generalstore:id/productAddCart")
+	private List<WebElement> addToCartButton;
+	
 	public void checkProductTitle(String expectedValue) {
 		waitForElementToAppear(10, productTitle, "text", "Products");
 		getProductTitle = productTitle.getText();
 		System.out.println("The title page is: " + getProductTitle);
 		compareString(getProductTitle, expectedValue);
+	}
+	
+	public void selectProduct(String chooseProduct) {
+		scrollToText(chooseProduct);
+		for(int i = 0; i < countProduct; i++) { 
+			String productName = productList.get(i).getText();
+			if(productName.equalsIgnoreCase(chooseProduct)) {
+				addToCartButton.get(i).click();
+				System.out.println("The product chose is: " + productName);
+				} 
+			}
 	}
 
 }
