2136866246b44d0b22291a5af6007cc9771fbcf1
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-14 10:24:46 +0200

    Created AppiumUtils.java

---
+++ src/main/java/utils/AppiumUtils.java
@@ -0,0 +1,15 @@
+package utils;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class AppiumUtils {
+	public AndroidDriver driver;
+
+	public AppiumUtils(AndroidDriver driver) {
+		this.driver = driver;
+	}
+
+	public void waitForElementToAppear() {
+		
+	}
+}
