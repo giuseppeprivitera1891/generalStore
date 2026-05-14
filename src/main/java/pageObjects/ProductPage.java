87ce55d3b28ff51330c21b4d638e1096c6a9783c
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-14 12:47:54 +0200

    Created ProductPage.java

---
+++ src/main/java/pageObjects/ProductPage.java
@@ -0,0 +1,13 @@
+package pageObjects;
+
+import io.appium.java_client.android.AndroidDriver;
+import utils.AndroidActions;
+
+public class ProductPage extends AndroidActions {
+
+	public ProductPage(AndroidDriver driver) {
+		super(driver);
+		
+	}
+
+}
