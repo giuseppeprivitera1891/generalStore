1077ad67ae4f3343a7caa03e322c71b61ba0d00b
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-14 10:33:41 +0200

    Added methods on AppiumUtils.java

--- src/main/java/utils/AppiumUtils.java
+++ src/main/java/utils/AppiumUtils.java
@@ -1,15 +1,28 @@
 package utils;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.appium.java_client.android.AndroidDriver;
 
 public class AppiumUtils {
-	public AndroidDriver driver;
+	AndroidDriver driver;
 
 	public AppiumUtils(AndroidDriver driver) {
 		this.driver = driver;
 	}
 
-	public void waitForElementToAppear() {
-		
+	public void waitForElementToAppear(Duration time, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated((By) element));
+	}
+	
+	public Double getFormattedAmount(String amount) {
+		Double price = Double.parseDouble(amount.substring(1));
+		return price;
 	}
 }
