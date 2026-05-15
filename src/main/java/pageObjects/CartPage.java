0e224e26217e69ea2401303ec2ee21fd53fb04fc
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-15 16:04:24 +0200

    Added checkCartTitle method

--- src/main/java/pageObjects/CartPage.java
+++ src/main/java/pageObjects/CartPage.java
@@ -1,8 +1,11 @@
 package pageObjects;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import utils.AndroidActions;
 
@@ -13,6 +16,17 @@ public class CartPage extends AndroidActions {
 		super(driver);
 		this.driver = driver;
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+	
+	@AndroidFindBy(id="com.androidsample.generalstore:id/toolbar_title")
+	private WebElement cartTitle;
+	
+	String getCartTitle;
+	
+	public void checkCartTitle(String expectedValue) {
+		getCartTitle = cartTitle.getText().trim();
+		System.out.println(getCartTitle);
+		Assert.assertEquals(getCartTitle, expectedValue);
 	}
 
 }
