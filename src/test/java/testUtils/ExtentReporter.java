ee17186193b6f04bc90d45ea0dbc0fd87e0b4b3c
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-20 14:25:07 +0200

    Added code for getReporterObject method

--- src/test/java/testUtils/ExtentReporter.java
+++ src/test/java/testUtils/ExtentReporter.java
@@ -1,5 +1,24 @@
 package testUtils;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class ExtentReporter {
+	ExtentReports extent;
+	ExtentSparkReporter reporter;
+	String path;
+	String tester = "Giuseppe Privitera";
+	
+	public ExtentReports getReporterObject() {
+		path = System.getProperty("user.dir") + "\\reports\\index.html";
+		reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("General Store report");
+		reporter.config().setDocumentTitle("Test results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", tester);
+		return extent;
+	}
 
 }
