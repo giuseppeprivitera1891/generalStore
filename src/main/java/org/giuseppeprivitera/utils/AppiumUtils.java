40893ddb09f2470855473f930cb7b2d1c5923653
giuseppeprivitera1891 <giuseppeprivitera1891@gmail.com>
2026-05-20 16:32:44 +0200

    Added getScreenshot method in AppiumUtils.java

--- src/main/java/org/giuseppeprivitera/utils/AppiumUtils.java
+++ src/main/java/org/giuseppeprivitera/utils/AppiumUtils.java
@@ -1,7 +1,11 @@
 package org.giuseppeprivitera.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,6 +16,8 @@ import io.appium.java_client.AppiumDriver;
 public class AppiumUtils {
 	WebDriverWait wait;
 	double price;
+	File source;
+	String destinationFile;
 
 	public void waitForElementToAppear(int time, WebElement element, String attribute, String value, AppiumDriver driver) {
 		wait = new WebDriverWait(driver, Duration.ofSeconds(time));
@@ -25,6 +31,13 @@ public class AppiumUtils {
 	public double getFormattedAmount(String amount) {
 		price = Double.parseDouble(amount.substring(1));
 		return price;
+	}
+	
+	public String getScreenshotPath(String testCaseName, AppiumDriver driver) throws IOException {
+		source = driver.getScreenshotAs(OutputType.FILE);
+		destinationFile = System.getProperty("user.dir") + "//reports//screenshots" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
 	}
 	
 }
